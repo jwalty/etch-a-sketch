6d8771a2ca03decff1d80513d27ee7ef4094f965
JWalty <therealjwalty@gmail.com>
2023-02-08 02:21:57 -0500

    Add race mode and limits to grid size

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const gridContainer = document.querySelector('#gridContainer');
 let gridDiagonalSize = 16;
 let canvasSize = 800;
+uniqueColored = 0;
+
+
+
+
 
 function createGrid(gridDiagonalSize) {
+
+    if (gridDiagonalSize < 0 || gridDiagonalSize > 100) {
+        window.alert("Try something a little more reasonable!");
+        location.reload();
+    }
+
+    uniqueColored = 0;
     gridContainer.innerHTML = '';
+    var startDate = new Date ();
+    var startTime = startDate.getTime();
     for (let i=0; i < gridDiagonalSize; i++) {
         let columnOfTiles = document.createElement('div');
         columnOfTiles.className = 'tileColumn';
@@ -11,6 +25,17 @@ function createGrid(gridDiagonalSize) {
             let gridTile = document.createElement('div');
             gridTile.className = 'gridTile';
             gridTile.addEventListener('mouseover', () => {
+                if (gridTile.style.backgroundColor != 'grey') {
+                    uniqueColored++;
+                    console.log(uniqueColored);
+                    if (uniqueColored == gridDiagonalSize * gridDiagonalSize) {
+                        var date_now = new Date();
+                        var time_now = date_now.getTime();
+                        var time_diff = time_now - startTime;
+                        var seconds_elapsed = time_diff / 1000;
+                        window.alert(`It took ${seconds_elapsed} seconds to fill in ${gridDiagonalSize * gridDiagonalSize} boxes! Great job!`);
+                    }
+                }
                 gridTile.style.backgroundColor = 'grey';
             });
             gridTile.style.height = (canvasSize / gridDiagonalSize).toString() + "px";
@@ -25,5 +50,5 @@ createGrid(gridDiagonalSize);
 
 const button = document.querySelector('#gridButton');
 button.addEventListener('click', () => { 
-    createGrid(window.prompt("Set a desired grid size!","16, 32, 64"));
+    createGrid(window.prompt("Set a desired grid size!","16"));
 });
